dfc9fafded43c957accf68031cef75f7fd5e1b73
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-03-14 14:26:15 +0530

    Initial commit

---
+++ longestWord.py
@@ -0,0 +1,14 @@
+def read_file(file_name):
+    with open(file_name) as file_handler:
+        longest_word=""
+        for each_row in file_handler:
+            word_list= each_row.rstrip().split()
+            for each_word in word_list:
+                if len(each_word) > len(longest_word):
+                    longest_word = each_word
+    print(f"The  longest word in the file: {longest_word}")
+def main():
+    file_name = input("Enter file name:")
+    read_file(file_name)
+if __name__ == "__main__":
+    main()

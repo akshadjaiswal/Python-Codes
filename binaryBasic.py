1207b70b7ec3db257e52c8221889eee81723ad32
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-03-17 22:11:41 +0530

    image folder

--- binaryBasic.py
+++ binaryBasic.py
@@ -1,5 +1,5 @@
 def main():
-    with open("React.jpg","rb") as existing_image, open("xyz.jpg","wb") as new_image:
+    with open("./Images/React.jpg","rb") as existing_image, open("xyz.jpg","wb") as new_image:
         for each_line_bytes in existing_image:
             new_image.write(each_line_bytes)
             

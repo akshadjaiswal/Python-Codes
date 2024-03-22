6ebb4810c9f38a22936634b95f3ccdefb9287e85
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-03-22 11:27:28 +0530

    Moving files

--- binaryBasic.py
+++
@@ -1,7 +0,0 @@
-def main():
-    with open("./Images/React.jpg","rb") as existing_image, open("xyz.jpg","wb") as new_image:
-        for each_line_bytes in existing_image:
-            new_image.write(each_line_bytes)
-            
-if __name__ == "__main__":
-    main()

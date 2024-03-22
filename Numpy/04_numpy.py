585300586b1441dede5d1548a5f4b98ff7a28873
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-03-22 11:49:17 +0530

    Numpy examples today

---
+++ Numpy/04_numpy.py
@@ -0,0 +1,5 @@
+import numpy as np
+print("\n Matrix is \n", np.linspace(1,2,5))
+print("\n Matrix is \n", np.linspace(2,5,2))
+print("\n Matrix is \n", np.linspace(10,4,5))
+print("\n Matrix is \n", np.linspace(30,10,5))
